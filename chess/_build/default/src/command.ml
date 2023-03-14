6733b5d92936464f52ae02076110e0bd02b4b046
Kevin Cui <k.cui1234@gmail.com>
2023-03-14 19:07:01 -0400

    remove _build folder

--- chess/_build/default/src/command.ml
+++
@@ -1,3 +0,0 @@
-let parse = raise (Failure "Unimplemented")
-let valid_actions = raise (Failure "Unimplemented")
-let move = raise (Failure "Unimplemented")
--- chess/_build/default/src/state.mli
+++
@@ -1,9 +0,0 @@
-(** Tracks the state of the chess game. This includes (but is not limited to)
-    the locations of each of the pieces, the number of moves in the game, a data
-    structure that stores the moves that have been played, a function that
-    determines if there is a check or checkmate, a function that determines if
-    En Passant can be played, and a way of tracking the pieces that are on (or
-    off) the board *)
-
-type board_state
-(** abstract type of board *)
